0f582e246c40cd08d3ede65587c96bc69867bfe9
Fernando Villalba <21fervillalbag@gmail.com>
2023-03-25 14:33:02 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return <Box color="suvap.darkGray">Hello world from vitejs</Box>;
+}
+
+export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  suvap: {
+    darkGray: "#333333",
+    lightGray: "#F0F0F0",
+  },
+};
+
+export const theme = extendTheme({ colors });
