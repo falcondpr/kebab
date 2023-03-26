febe89716ab56c16dd6dad23a0942b7298e8450e
Fernando Villalba <21fervillalbag@gmail.com>
2023-03-26 17:33:09 -0400

    feat: ProductCard component ui dev

---
+++ data/products.ts
@@ -0,0 +1,18 @@
+export const PRODUCTS = [
+  {
+    id: "1",
+    image: "https://bit.ly/3ZbmPlW",
+  },
+  {
+    id: "2",
+    image: "https://bit.ly/3ZbmPlW",
+  },
+  {
+    id: "3",
+    image: "https://bit.ly/3ZbmPlW",
+  },
+  {
+    id: "4",
+    image: "https://bit.ly/3ZbmPlW",
+  },
+];
