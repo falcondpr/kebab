df742f35246e3d74917b069d5d16e18af37e7ff9
Fernando Villalba <21fervillalbag@gmail.com>
2023-03-26 19:23:00 -0400

    feat: Logic basic press product and show pinterest options to pin

--- data/products.ts
+++ data/products.ts
@@ -2,17 +2,21 @@ export const PRODUCTS = [
   {
     id: "1",
     image: "https://bit.ly/3ZbmPlW",
+    name: "Blazer brown",
   },
   {
     id: "2",
     image: "https://bit.ly/3ZbmPlW",
+    name: "Blazer brown",
   },
   {
     id: "3",
     image: "https://bit.ly/3ZbmPlW",
+    name: "Blazer brown",
   },
   {
     id: "4",
     image: "https://bit.ly/3ZbmPlW",
+    name: "Blazer brown",
   },
 ];
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,28 +1,81 @@
 import { Box, Image, Text } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+
 import TagUser from "./TagUser";
 
-export default function ProductCard({ product }: any) {
-  return (
-    <Box>
-      <Box rounded="4px" overflow="hidden">
-        <Image
-          h="205px"
-          src={product.image}
-          w="full"
-          alt=""
-          objectFit="cover"
-        />
-      </Box>
+interface IProductSelect {
+  status: boolean;
+  id: string | null;
+}
 
-      <Box mt="5px">
-        <Text
-          fontSize="18px"
-          fontWeight="semibold"
-          color="suvap.darkGray"
-        >
-          Brown Blazer
-        </Text>
-        <TagUser />
+interface IProductCard {
+  product: { id: string; image: string; name: string };
+  productSelect: { status: boolean; id: string | null };
+  setProductSelect: (value: IProductSelect) => void;
+}
+
+export default function ProductCard({
+  product,
+  productSelect,
+  setProductSelect,
+}: IProductCard) {
+  return (
+    <Box
+      position="relative"
+      onClick={() => {
+        if (productSelect.id) {
+          setProductSelect({ status: false, id: null });
+        } else {
+          setProductSelect({ status: true, id: product.id });
+        }
+      }}
+      opacity={
+        productSelect.status
+          ? product.id === productSelect.id
+            ? "1"
+            : "0.3"
+          : "1"
+      }
+    >
+      <Box
+        display={
+          productSelect.status
+            ? product.id === productSelect.id
+              ? "block"
+              : "none"
+            : "none"
+        }
+        position="absolute"
+        top="50%"
+        left="0"
+        transform="translateY(-50%)"
+        w="full"
+        h="60px"
+        bgColor="red.400"
+        zIndex="20"
+      ></Box>
+
+      <Box>
+        <Box rounded="4px" overflow="hidden">
+          <Image
+            h="205px"
+            src={product.image}
+            w="full"
+            alt=""
+            objectFit="cover"
+          />
+        </Box>
+
+        <Box mt="5px">
+          <Text
+            fontSize="18px"
+            fontWeight="semibold"
+            color="suvap.darkGray"
+          >
+            Brown Blazer
+          </Text>
+          <TagUser />
+        </Box>
       </Box>
     </Box>
   );
