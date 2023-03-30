5204f7d9b44d4141e627b8ab91a9664ae3481910
Fernando Villalba <fernando.villalba@clt.com.py>
2023-03-30 09:36:10 -0400

    feat: install storybook basic config

---
+++ .storybook/main.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@chakra-ui/storybook-addon",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    emotionAlias: false,
+    storyStoreV7: true,
+  },
+};
