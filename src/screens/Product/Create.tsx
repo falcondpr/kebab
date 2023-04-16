23387b268913cde8dd323aa3b03b2ccc91da4171
Fernando Villalba <21fervillalbag@gmail.com>
2023-04-16 13:21:33 -0400

    feat: addying validation a radio buttons

--- src/screens/Product/Create.tsx
+++ src/screens/Product/Create.tsx
@@ -1,7 +1,9 @@
 import {
   Box,
+  Flex,
   FormControl,
   FormErrorMessage,
+  HStack,
   Input,
   Radio,
   RadioGroup,
@@ -18,9 +20,11 @@ import {
 
 import { BackButton } from "@/components";
 import { Button } from "@/ui";
+import { useRef } from "react";
 
 const CreateProductSchema = Yup.object().shape({
   name: Yup.string().min(5, "Es muy corto!").required("Es requerido"),
+  status: Yup.string().required("Es requerido"),
 });
 
 const initialValuesCreateProduct = {
@@ -29,6 +33,9 @@ const initialValuesCreateProduct = {
 };
 
 export default function CreateProduct() {
+  const refFirstButton: any = useRef();
+  const refSecondButton: any = useRef();
+
   const handleCreateProduct = async (
     values: any,
     resetForm: () => void
@@ -41,6 +48,7 @@ export default function CreateProduct() {
   return (
     <Box>
       <BackButton goBackRoute="/" title="Crear publicacion" />
+
       <Formik
         validationSchema={CreateProductSchema}
         initialValues={initialValuesCreateProduct}
@@ -68,23 +76,80 @@ export default function CreateProduct() {
                 />
               </FormControl>
 
+              <Box my="20px">{JSON.stringify(values)}</Box>
+
+              <HStack mt="20px" spacing={5}>
+                <Button
+                  bgColor={
+                    values.status === "1"
+                      ? "suvap.darkGray"
+                      : "suvap.lightGray"
+                  }
+                  color={
+                    values.status === "1"
+                      ? "suvap.lightGray"
+                      : "suvap.darkGray"
+                  }
+                  onClick={() => refFirstButton.current?.click()}
+                >
+                  First
+                </Button>
+                <Button
+                  bgColor={
+                    values.status === "2"
+                      ? "suvap.darkGray"
+                      : "suvap.lightGray"
+                  }
+                  color={
+                    values.status === "2"
+                      ? "suvap.lightGray"
+                      : "suvap.darkGray"
+                  }
+                  onClick={() => refSecondButton.current?.click()}
+                >
+                  Second
+                </Button>
+              </HStack>
+
               <Field name="status">
-                {({ field, form }: FieldProps) => {
+                {({ field }: FieldProps) => {
                   const { onChange, ...rest } = field;
 
                   return (
                     <FormControl
                       id="status"
-                      isInvalid={!!form.status && !!form.status}
+                      isInvalid={!!errors.status && !!touched.status}
                     >
+                      <ErrorMessage
+                        name="status"
+                        component={() => (
+                          <FormErrorMessage>
+                            {errors.status}
+                          </FormErrorMessage>
+                        )}
+                      />
+
                       <RadioGroup id="status" {...rest}>
                         <Stack>
-                          <Radio onChange={onChange} value="1">
+                          <Radio
+                            name="status"
+                            ref={refFirstButton}
+                            display="none"
+                            onChange={onChange}
+                            value="1"
+                          >
                             First
                           </Radio>
                         </Stack>
                         <Stack>
-                          <Radio onChange={onChange} value="2">
+                          <Radio
+                            name="status"
+                            // onBlur={handleBlur}
+                            ref={refSecondButton}
+                            display="none"
+                            onChange={onChange}
+                            value="2"
+                          >
                             Second
                           </Radio>
                         </Stack>
