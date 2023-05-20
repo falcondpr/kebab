5c8c31a1825928763f502bb7c25b65c3f9abe0fe
Fernando Villalba <21fervillalbag@gmail.com>
2023-05-19 21:59:12 -0400

    feat: list buttons profile screen

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -8,6 +8,11 @@ import { getUser } from "../services/user";
 import { useAuthStore } from "../store";
 import { colors } from "../styles/theme";
 import { Heading, Text } from "../ui";
+import {
+  settingsInfoProfile,
+  settingsInfoTerms,
+  settingsInfoUser,
+} from "../data/settings";
 
 import LogoutIcon from "../assets/icons/logout-icon.svg";
 import VerifiedIcon from "../assets/icons/verified-icon.svg";
@@ -26,7 +31,7 @@ export default function Profile({ navigation }: any) {
 
   return (
     <MainLayout routeName="ProfileScreen" navigation={navigation}>
-      <ProfileContainer>
+      <ProfileContainer showsVerticalScrollIndicator={false}>
         <ProfileBanner
           source={require("../assets/images/banner-profile.png")}
         >
@@ -80,12 +85,83 @@ export default function Profile({ navigation }: any) {
             <Text fontSize="14px">@{userInfo?.data.username}</Text>
           )}
         </ProfileInfoContainer>
+
+        <ProfileMainSettings>
+          <View style={{ marginBottom: 10 }}>
+            <Text
+              marginBottom="10px"
+              color={colors.primary}
+              fontSize="18px"
+            >
+              Informacion de usuario
+            </Text>
+
+            {settingsInfoUser.map((info) => (
+              <ProfileSettingsItem key={info.id}>
+                <info.icon />
+                <Text marginLeft="10px" fontSize="20px">
+                  {info.text}
+                </Text>
+              </ProfileSettingsItem>
+            ))}
+          </View>
+
+          <View style={{ marginBottom: 10 }}>
+            <Text
+              marginBottom="10px"
+              color={colors.primary}
+              fontSize="18px"
+            >
+              Editar perfil
+            </Text>
+
+            {settingsInfoProfile.map((info) => (
+              <ProfileSettingsItem key={info.id}>
+                <info.icon />
+                <Text marginLeft="10px" fontSize="20px">
+                  {info.text}
+                </Text>
+              </ProfileSettingsItem>
+            ))}
+          </View>
+
+          <View>
+            <Text
+              marginBottom="10px"
+              color={colors.primary}
+              fontSize="18px"
+            >
+              Terminos y condiciones
+            </Text>
+
+            {settingsInfoTerms.map((info) => (
+              <ProfileSettingsItem key={info.id}>
+                <info.icon />
+                <Text marginLeft="10px" fontSize="20px">
+                  {info.text}
+                </Text>
+              </ProfileSettingsItem>
+            ))}
+          </View>
+        </ProfileMainSettings>
+
+        <EmptyBottomSpace />
       </ProfileContainer>
     </MainLayout>
   );
 }
 
-const ProfileContainer = styled.View``;
+const EmptyBottomSpace = styled.View`
+  height: 60px;
+`;
+
+const ProfileSettingsItem = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProfileContainer = styled.ScrollView``;
 
 const ProfileBanner = styled.ImageBackground`
   height: 120px;
@@ -122,4 +198,8 @@ const ProfileButtonLogout = styled.TouchableOpacity`
 const ProfileInfoContainer = styled.View`
   margin-top: 60px;
   padding: 0 20px;
+`;
+
+const ProfileMainSettings = styled.View`
+  padding: 20px;
 `;
