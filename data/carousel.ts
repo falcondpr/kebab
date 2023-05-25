50515ad6fb9fdcdcc9399edac7e4a0d5f3539fb2
Fernando Villalba <21fervillalbag@gmail.com>
2023-05-24 20:12:51 -0400

    feat: carousel react native reanimated

---
+++ data/carousel.ts
@@ -0,0 +1,26 @@
+export const carouselData = [
+  {
+    id: 1,
+    uri: "https://shorturl.at/beVX4",
+  },
+  {
+    id: 2,
+    uri: "https://shorturl.at/bcnOP",
+  },
+  {
+    id: 3,
+    uri: "https://shorturl.at/wAGI4",
+  },
+  {
+    id: 4,
+    uri: "https://shorturl.at/zJQ57",
+  },
+  {
+    id: 5,
+    uri: "https://shorturl.at/cegyM",
+  },
+  {
+    id: 6,
+    uri: "https://shorturl.at/bcnOP",
+  },
+];
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,97 +1,38 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import styled from "styled-components/native";
-import PagerView from "react-native-pager-view";
+import { FlatList } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 
-import BellIcon from "../assets/icons/bell-icon.svg";
 import MainLayout from "../layout/Main";
-import { colors } from "../styles/theme";
-import { Heading, Text } from "../ui";
+import { carouselData } from "../data/carousel";
+import CarouselItem from "../components/CarouselItem";
 
 export default function Home({ navigation }: any) {
+  const scrollX = useSharedValue(0);
+
+  const onScroll = (e: any) => {
+    scrollX.value = e.nativeEvent.contentOffset.x;
+  };
+
   return (
     <MainLayout routeName="HomeScreen" navigation={navigation}>
-      {/* <PagerView style={styles.pagerView} initialPage={0} orientation="vertical">
-        <View style={{ backgroundColor: "lightblue", flex: 1 }} key="1">
-          <Text>First page</Text>
-        </View>
-        <View style={{ backgroundColor: "pink", flex: 1 }} key="2">
-          <Text>Second page</Text>
-        </View>
-      </PagerView> */}
-
-      {/* <HeaderContainer>
-        <Heading
-          color={colors.primary}
-          fontSize="24px"
-          textTransform="uppercase"
-        >
-          Sura
-        </Heading>
-        <ButtonNotification>
-          <BellIcon />
-        </ButtonNotification>
-      </HeaderContainer>
-
-      <HeroImageContainer>
-        <HeroImage
-          source={require("../assets/images/header-home.png")}
-        />
-      </HeroImageContainer> */}
-
-      {/* <Button>
-        <ButtonText
-          style={{
-            height: 50,
-            backgroundColor: "#333",
-            color: "#fff",
-          }}
-          onPress={() => navigation.navigate("Auth")}
-        >
-          go to auth
-        </ButtonText>
-
-        <ButtonText
-          style={{ margin: 20, padding: 20, backgroundColor: "#3fd" }}
-          onPress={() => user ? console.log(user) : null}
-        >
-          obtener usuario logueado
-        </ButtonText>
-      </Button> */}
+      <FlatList
+        horizontal
+        onScroll={onScroll}
+        scrollEventThrottle={18}
+        data={carouselData}
+        bounces={false}
+        style={{ margin: 14, height: 300, flexGrow: 0 }}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => (
+          <CarouselItem
+            uri={item.uri}
+            scrollX={scrollX}
+            index={index}
+            dataLength={carouselData.length}
+          />
+        )}
+      />
     </MainLayout>
   );
 }
-
-const Button = styled.TouchableOpacity``;
-
-const ButtonText = styled.Text``;
-
-const HeaderContainer = styled.View`
-  padding: 20px;
-  /* padding-bottom: 16px; */
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const HeroImageContainer = styled.View``;
-
-const HeroImage = styled.ImageBackground`
-  height: 225px;
-  margin: 0 20px;
-  object-fit: cover;
-`;
-
-const ButtonNotification = styled.TouchableOpacity`
-  padding: 8px;
-  margin-top: -3px;
-`;
-
-const styles = StyleSheet.create({
-  pagerView: {
-    flex: 1,
-  },
-  page: {
-    flex: 1,
-  },
-});
